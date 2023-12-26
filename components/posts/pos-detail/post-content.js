1d81c244c84616b4dcaeb165553b4edbbc719f59
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-26 21:08:08 +0530

    prerendering data in allpost featurepost  and postdetail page

--- components/posts/pos-detail/post-content.js
+++ components/posts/pos-detail/post-content.js
@@ -3,24 +3,19 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
 
 
-const DUMMY_POSTS=
-    {
-      slug:'getting-started-nextjs-1',
-     title: 'Nextjs',
-     image:'getting-started-nextjs.png',
-     content:`# This is the first post`,
-       date: '2023-12-26'
-    }
 
 
-function PostContent() {
 
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+function PostContent(props) {
+
+    const {post} = props;
+    
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return (
         <article className={classes.content}>
-        <PostHeader title={DUMMY_POSTS.title} image={imagePath}/>
+        <PostHeader title={post.title} image={imagePath}/>
         <ReactMarkDown>
-        {DUMMY_POSTS.content}
+        {post.content}
         </ReactMarkDown>
         </article>
     )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,13 +1,39 @@
 import PostContent from "../../components/posts/pos-detail/post-content"
+import {getPostData, getPostsFile} from "../../lib/post-util"
+function PostDetailsPage(props){
 
-function PostDetailsPage(){
-
+    
     return(
         <>
        
-        <PostContent/ >
+        <PostContent post={props.post}/ >
         </>
     )
+}
+
+export function getStaticProps(context){
+    const {params} = context;
+    const {slug} = params
+
+   const postData = getPostData(slug)
+
+   return {
+    props:{
+        post:postData
+    },
+    revalidate:600
+   }
+}
+
+export function getStaticPaths(){
+
+  const postFilename =  getPostsFile();
+  const slugs = postFilename.map(fileName=>fileName.replace(/\.md$/,''))
+
+    return{
+        paths:slugs.map(slug=>({params:{slug:slug}})),
+        fallback:true
+    }
 }
 
 export default PostDetailsPage;
