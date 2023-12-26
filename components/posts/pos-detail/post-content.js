eff12119a2b2e46e0c6d833888b995f2462c59d7
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-26 21:59:47 +0530

    update

--- components/posts/pos-detail/post-content.js
+++ components/posts/pos-detail/post-content.js
@@ -1,21 +1,34 @@
 import ReactMarkDown from "react-markdown"
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
-
+import Image from "next/image";
 
 
 
 
 function PostContent(props) {
-
     const {post} = props;
+    const imagePath = `/images/posts/${post?.slug}/${post?.image}`
+
+    const customRenderers = {
+        image(image) {
+            return  (<Image 
+            src={`/images/posts/${post.slug}/${image.src}`} 
+            alt={image.alt} 
+            width={600} 
+            height={300}
+            />
+            );
+        }
+    }
     
-    const imagePath = `/images/posts/${post.slug}/${post.image}`
+    
+   
     return (
         <article className={classes.content}>
-        <PostHeader title={post.title} image={imagePath}/>
+        <PostHeader title={post?.title} image={imagePath}/>
         <ReactMarkDown>
-        {post.content}
+        {post?.content}
         </ReactMarkDown>
         </article>
     )
